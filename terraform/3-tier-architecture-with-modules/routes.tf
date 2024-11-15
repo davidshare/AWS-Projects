c7df25642d3fb55a989c00c88de4466b158ed7f5
David Essien <davidessienshare@gmail.com>
2024-11-15 23:17:08 +0100

    update security groups and routes

--- terraform/3-tier-architecture-with-modules/routes.tf
+++ terraform/3-tier-architecture-with-modules/routes.tf
@@ -1,7 +1,40 @@
-variable "route_tables" {}
-variable "subnets_route_table_association" {}
-variable "internet_gateway_routes" {}
-variable "nat_gateway_routes" {}
+# Variable for route tables
+variable "route_tables" {
+  description = "Map of route table configurations"
+  type = map(object({
+    vpc  = string
+    tags = map(string)
+  }))
+}
+
+# Variable for subnet to route table associations
+variable "subnets_route_table_association" {
+  description = "Map of subnet to route table associations"
+  type = map(object({
+    subnet      = string
+    route_table = string
+  }))
+}
+
+# Variable for Internet Gateway routes
+variable "internet_gateway_routes" {
+  description = "Map of routes for Internet Gateway"
+  type = map(object({
+    route_table = string
+    cidr        = string
+    gateway     = string
+  }))
+}
+
+# Variable for NAT Gateway routes
+variable "nat_gateway_routes" {
+  description = "Map of routes for NAT Gateway"
+  type = map(object({
+    route_table = string
+    cidr        = string
+    gateway     = string
+  }))
+}
 
 # Route tables
 module "route_table" {
@@ -22,7 +55,7 @@ module "subnets_route_table_associations" {
   subnet_id      = module.subnet[each.value.subnet].subnet_id
   route_table_id = module.route_table[each.value.route_table].route_table_id
 
-  depends_on = [ module.subnet, module.var.route_table ]
+  depends_on = [module.subnet, module.route_table]
 }
 
 # Routes for Internet Gateway
@@ -35,7 +68,7 @@ module "internet_gateway_routes" {
   destination_cidr_block = each.value.cidr
   gateway_id             = module.internet_gateway[each.value.gateway].internet_gateway_id
 
-  depends_on = [ module.internet_gateway, module.route_table ]
+  depends_on = [module.internet_gateway, module.route_table]
 }
 
 # Routes for NAT
@@ -48,5 +81,5 @@ module "nat_gateway_routes" {
   destination_cidr_block = each.value.cidr
   nat_gateway_id         = module.nat_gateway[each.value.gateway].nat_gateway_id
 
-  depends_on = [ module.nat_gateway, module.route_table ]
+  depends_on = [module.nat_gateway, module.route_table]
 }
